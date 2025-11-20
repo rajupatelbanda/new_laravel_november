98f3aa05c53bf5eade2c9dddec6671275789019e
RajuBanda <100689689+rajupatelbanda@users.noreply.github.com>
2025-11-20 12:48:36 +0530

    Add routes for products and categories

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return "this is contact Page";
 });
+
+
+// Route::get('/products', function (Request $request) {
+//     return response()->json([
+//         'products' => \App\Models\Product::all()
+//     ]);
+// });
+
+ Route::get('/products', function (Request $request) {
+$products=\App\Models\Product::all();
+    return view('admin.product.index',compact('products'));
+ });
+
+Route::get('/categories', function (Request $request) {
+    $categories=\App\Models\Category::all();
+    return view('admin.category.index',compact('categories'));
+});
+
+
+
+
